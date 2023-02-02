e8a0552190a650e9651be1e1abebc7633c8319ad
ousttrue <ousttrue@gmail.com>
2023-02-02 14:36:47 +0900

    separate

---
+++ example/GuiApp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class GuiApp {
+  // Our state
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  float clear_color_[4] = {0.45f, 0.55f, 0.60f, 1.00f};
+
+public:
+  float clear_color[4] = {};
+  GuiApp(struct GLFWwindow *window, const char *glsl_version);
+  ~GuiApp();
+  void UpdateGui();
+  void RenderGui();
+};
