849f19c5000577d4ee6f814be84c334a26952e46
ousttrue <ousttrue@gmail.com>
2023-04-30 00:14:03 +0900

    update DxCubeStereoRenderer

--- cuber/src/dx/DxCubeStereoRenderer.cpp
+++ cuber/src/dx/DxCubeStereoRenderer.cpp
@@ -19,13 +19,14 @@ struct VSInput {
     float4 Row1: ROW1;
     float4 Row2: ROW2;
     float4 Row3: ROW3;
-    float4 Color: COLOR0;
+    float4 positiveXyzFlag: FACE0;
+    float4 negativeXyzFlag: FACE1;
     uint instId : SV_InstanceID;
 };
 struct VSOutput {
     float4 Pos : SV_POSITION;
-    float4 color: COLOR0;
     float4 uv_barycentric: TEXCOORD;
+    uint3 paletteFlagFlag: COLOR;
     uint viewId : SV_RenderTargetArrayIndex;
 };
 
@@ -40,13 +41,63 @@ float4x4 transform(float4 r0, float4 r1, float4 r2, float4 r3)
 }
 
 VSOutput vs_main(VSInput IN) {
-    VSOutput OUT;
-    OUT.Pos = mul(mul(IN.Pos, transform(IN.Row0, IN.Row1, IN.Row2, IN.Row3)), ViewProjection[IN.instId % 2]);
-    OUT.color = IN.Color;
-    OUT.uv_barycentric = IN.uv_barycentric;
-    OUT.viewId = IN.instId % 2;
-    return OUT;
+  VSOutput OUT;
+  OUT.Pos = mul(mul(float4(IN.Pos.xyz, 1), transform(IN.Row0, IN.Row1, IN.Row2, IN.Row3)), ViewProjection[IN.instId % 2]);
+  OUT.uv_barycentric = IN.uv_barycentric;
+  OUT.viewId = IN.instId % 2;
+  if(IN.pos.w==0.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.positiveXyzFlag.x, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+  else if(IN.pos.w==1.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.positiveXyzFlag.y, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+  else if(IN.pos.w==2.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.positiveXyzFlag.z, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+  else if(IN.pos.w==3.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.negativeXyzFlag.x, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+  else if(IN.pos.w==4.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.negativeXyzFlag.y, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+  else if(IN.pos.w==5.0)
+  {
+    OUT.paletteFlagFlag = uint3(IN.negativeXyzFlag.z, 
+      IN.positiveXyzFlag.w,
+      IN.negativeXyzFlag.w);
+  }
+
+  return OUT;
 }
+
+cbuffer c0
+{
+  float4 colors[64];
+  float4 textures[64];
+};
+Texture2D texture0;
+SamplerState sampler0;
+Texture2D texture1;
+SamplerState sampler1;
+Texture2D texture2;
+SamplerState sampler2;
+Texture2D texture3;
+SamplerState sampler3;
 
 float grid (float2 vBC, float width) {
   float3 bary = float3(vBC.x, vBC.y, 1.0 - vBC.x - vBC.y);
@@ -57,7 +108,30 @@ float grid (float2 vBC, float width) {
 
 float4 ps_main(VSOutput IN) : SV_TARGET {
   float value = grid(IN.uv_barycentric.zw, 1.0);
-  return IN.color * float4(value, value, value, 1.0);
+  float4 border = float4(value, value, value, 1.0);
+  float4 color = colors[IN.paletteFlagFlag.x];
+  float4 texel;
+  if(textures[IN.paletteFlagFlag.x].x==0.0)
+  {
+    texel = texture0.Sample(sampler0, IN.uv_barycentric.xy);
+  }
+  else if(textures[IN.paletteFlagFlag.x].x==1.0)
+  {
+    texel = texture1.Sample(sampler1, IN.uv_barycentric.xy);
+  }
+  else if(textures[IN.paletteFlagFlag.x].x==2.0)
+  {
+    texel = texture2.Sample(sampler2, IN.uv_barycentric.xy);
+  }
+  else if(textures[IN.paletteFlagFlag.x].x==3.0)
+  {
+    texel = texture3.Sample(sampler3, IN.uv_barycentric.xy);
+  }
+  else{
+    texel = float4(1, 1, 1, 1);
+  }
+
+  return texel * color * border;
 }
 )";
 
@@ -70,6 +144,9 @@ struct DxCubeStereoRendererImpl
   winrt::com_ptr<ID3D11Buffer> instance_buffer_;
   std::shared_ptr<grapho::dx11::Drawable> drawable_;
   winrt::com_ptr<ID3D11Buffer> constant_buffer_;
+
+  Pallete pallete_;
+  winrt::com_ptr<ID3D11Buffer> pallete_buffer_;
 
   DxCubeStereoRendererImpl(const winrt::com_ptr<ID3D11Device>& device)
     : device_(device)
@@ -111,6 +188,11 @@ struct DxCubeStereoRendererImpl
 
     constant_buffer_ = grapho::dx11::CreateConstantBuffer(
       device_, sizeof(DirectX::XMFLOAT4X4) * 2, nullptr);
+    assert(constant_buffer_);
+
+    pallete_buffer_ =
+      grapho::dx11::CreateConstantBuffer(device_, sizeof(pallete_), &pallete_);
+    assert(pallete_buffer_);
   }
 
   void Render(const float projection[16],
@@ -157,8 +239,10 @@ struct DxCubeStereoRendererImpl
     }
     context->UpdateSubresource(constant_buffer_.get(), 0, NULL, vp, 0, 0);
 
-    ID3D11Buffer* cb[]{ constant_buffer_.get() };
-    context->VSSetConstantBuffers(0, 1, cb);
+    ID3D11Buffer* vscb[]{ constant_buffer_.get() };
+    context->VSSetConstantBuffers(0, 1, vscb);
+    ID3D11Buffer* pscb[]{ pallete_buffer_.get() };
+    context->PSSetConstantBuffers(0, 1, pscb);
 
     // 2
     drawable_->DrawInstance(context, CUBE_INDEX_COUNT, instanceCount);
