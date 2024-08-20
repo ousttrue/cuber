65f6a969237b3b7c304de6c6627730ff7fd6f2ba
ousttrue <ousttrue@gmail.com>
2024-08-20 11:08:50 +0900

    WIP build.zig

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const grapho = @import("grapho");
 
 const cflags = [_][]const u8{
     "-std=c++20",
@@ -38,11 +39,16 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const grapho_dep = b.dependency("grapho", .{});
-    const glfw_dep = grapho_dep.builder.dependency("glfw", .{});
-    const directxmath_dep = b.dependency("directxmath", .{});
-    const glew_dep = b.dependency("glew", .{});
-    const imgui_dep = b.dependency("imgui", .{});
-
+    const grapho_opts = grapho.MakeOptions{
+        .b = b,
+        .target = target,
+        .optimize = optimize,
+        .dep_builder = grapho_dep.builder,
+    };
+    // const glfw_dep = grapho_dep.builder.dependency("glfw", .{});
+    // const directxmath_dep = b.dependency("directxmath", .{});
+    // const glew_dep = b.dependency("glew", .{});
+    // const imgui_dep = b.dependency("imgui", .{});
     const asio_dep = b.dependency("asio", .{});
     const meshutils_dep = b.dependency("meshutils", .{});
 
@@ -52,28 +58,29 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
         });
+
+        // deps
         exe.linkLibCpp();
+        grapho.make_grapho(grapho_opts, cflags).inject_to(exe);
+        grapho.make_directxmath(grapho_opts).inject_to(exe);
+        grapho.make_glew(grapho_opts, cflags).inject_to(exe);
+        grapho.make_glfw(grapho_opts, cflags).inject_to(exe);
+        grapho.make_imgui(grapho_opts, cflags).inject_to(exe);
+
+        // srcs
         exe.addCSourceFiles(.{
             .root = b.path(example.root),
             .files = example.files,
             .flags = &cflags,
         });
-        exe.addIncludePath(directxmath_dep.path("Inc"));
-        exe.addIncludePath(glew_dep.path("include"));
-        exe.addIncludePath(grapho_dep.path("src"));
-        exe.addIncludePath(imgui_dep.path(""));
-        exe.addIncludePath(imgui_dep.path("backends"));
         exe.addIncludePath(asio_dep.path("asio/include"));
         exe.addIncludePath(meshutils_dep.path("Source"));
-        exe.addIncludePath(glfw_dep.path("include"));
-
         exe.addIncludePath(b.path("cuber/include"));
         exe.addCSourceFiles(.{
             .files = &.{
                 "cuber/src/gl3/GlCubeRenderer.cpp",
             },
         });
-
         exe.addIncludePath(b.path("example/bvhutil"));
         exe.addCSourceFiles(.{
             .root = b.path("example/bvhutil"),
@@ -81,9 +88,10 @@ pub fn build(b: *std.Build) void {
             .flags = &cflags,
         });
 
+        // install
         const install = b.addInstallArtifact(exe, .{});
         b.getInstallStep().dependOn(&install.step);
-
+        // run
         const run_cmd = b.addRunArtifact(exe);
         run_cmd.step.dependOn(b.getInstallStep());
         if (b.args) |args| {
